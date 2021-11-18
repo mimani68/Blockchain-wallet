52e55e454da41011483988523ead0b87cc53697b
mahdi <imani@iran.ir>
2021-11-18 10:50:38 +0330

    feat: create multi address and recover one

--- hdwallet.go
+++ hdwallet.go
@@ -7,9 +7,21 @@ import (
 	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 )
 
-func main() {
-	mnemonic := "tonight capital wing name satisfy mule grace next mansion very retreat emotion"
+func recoverAccount(mnemonic string, accountNumber int) {
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pathHD := hdwallet.MustParseDerivationPath(fmt.Sprintf("m/44'/60'/0'/0/%d", accountNumber))
+	account, err := wallet.Derive(pathHD, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	priv, _ := wallet.PrivateKeyHex(account)
+	fmt.Printf("%s\n", priv)
+}
 
+func createDepositeAddress(mnemonic string) {
 	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	if err != nil {
 		log.Fatal(err)
@@ -40,5 +52,31 @@ func main() {
 
 		fmt.Printf("Public key in hex: %s\n", publicKey)
 	}
+}
 
+func accumolativeBalanceOfWallet(mnemonic string) {
+	fmt.Println("Comming soon")
+}
+
+func main() {
+	//
+	// This password most store permamanetly, do not share this one.
+	// $ go run mnemonic.go
+	//
+	mnemonic := "tonight capital wing name satisfy mule grace next mansion very retreat emotion"
+
+	//
+	// You can create unlimit account for each wallet
+	//
+	createDepositeAddress(mnemonic)
+
+	//
+	// With above paswrod we can restore specidif account
+	//
+	recoverAccount(mnemonic, 52)
+
+	//
+	// Aggregate remain value in each address wallet
+	//
+	accumolativeBalanceOfWallet(mnemonic)
 }
